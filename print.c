110644429e1390aa46ab422afed64f8ba8857616
amberlcole <amber.l.cole@asu.edu>
2014-02-23 20:54:23 -0700

    Update print.c

--- print.c
+++ print.c
@@ -8,41 +8,32 @@ void print_line(char line[], char source_name_to_print[], char date_to_print[])
     char *save_chp = NULL;
     static int line_count = MAX_LINES_PER_PAGE;
     
-    for (int i=0; i < MAX_PRINT_LINE_LENGTH; i++)
-    {
-        printf("%c", line);
-        if ((i+1)%10==0)
-        {
-            line_count--;
-            printf("\n");
-        }
-    }
-    if (line_count ==0)
-    {
-        print_page_header(source_name_to_print, date_to_print)
-    }
-    
-/*    
     if (++line_count > MAX_LINES_PER_PAGE)
     {
         //Needs to start a new page and add a header to the top
-        print_page_header( char source_name_to_print[], char date_to_print[] );
+        line_count = 0;
+        print_page_header( source_name_to_print, date_to_print );
     }
     if (strlen(line) > MAX_PRINT_LINE_LENGTH) 
     {
         // Checking to see if the line is too long, if it is it needs to be 
-        trucated, then restored to it's normal length when printed
+        //trucated
+        save_chp = &line[0];
+        save_ch = str( line );
     }
     if (save_chp)
     {
-        // Missing Code Here 
+        strncpy( line, line, MAX_PRINT_LINE_LENGTH );
     }
-    // Missing Code Here 
+    
+    printf("%s", line );
+    
     if (save_chp)
     {
-        // Missing Code Here 
+        // restores to it's normal length when printed
+        strncpy( line, save_chp, save_ch );
     }
-    */
+    
 }
 static void print_page_header(char source_name[], char date[])
 {
